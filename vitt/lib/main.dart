1176e1bbd85e5b12b3afe16cafcba2457af5c9a1
Abhishek Rai <abhishek.rai.cewit@gmail.com>
2019-02-25 01:14:51 -0500

    Adding bottom nav bar and cleaned up the ui

--- vitt/lib/main.dart
+++ vitt/lib/main.dart
@@ -20,7 +20,8 @@ class MyApp extends StatelessWidget {
         // is not restarted.
           primaryColorDark: Colors.black,
           brightness: Brightness.dark,
-          accentColor: Colors.green
+          accentColor: Colors.green,
+          primarySwatch: Colors.green
       ),
       home: MyHomePage(title: 'Vitt'),
     );
@@ -46,6 +47,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -61,32 +63,34 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Straw Man',
-            ),
-          ],
-        ),
+      body: _buildBody(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.assessment),
+              title: Text('Stocks')
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.attach_money),
+              title: Text('FX')
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.broken_image),
+              title: Text('Crypto')
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
+  }
+
+  Widget _buildBody() {
+    return Container();
+  }
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 }
